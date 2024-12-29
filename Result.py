67cb410ae91c7f37aac623ed5b859183511a32ce
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-29 12:06:15 +0600

    Create Result.py

---
+++ Result.py
@@ -0,0 +1,21 @@
+Latitude  Longitude Response Unit
+0   23.8103    90.4125        Police
+1   23.8145    90.4145          Fire
+2   23.8200    90.4160     Ambulance
+3   23.8300    90.4200        Police
+4   23.8350    90.4220          Fire
+Accuracy: 0.00%
+Classification Report:
+              precision    recall  f1-score   support
+
+        Fire       0.00      0.00      0.00       1.0
+      Police       0.00      0.00      0.00       0.0
+
+    accuracy                           0.00       1.0
+   macro avg       0.00      0.00      0.00       1.0
+weighted avg       0.00      0.00      0.00       1.0
+
+Confusion Matrix:
+[[0 1]
+ [0 0]]
+নিকটতম রেসপন্স ইউনিট: Ambulance
